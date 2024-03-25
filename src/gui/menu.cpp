b1004fd187f73147524d87809d4133806c1ab9d3
v0idp <voidp@protonmail.com>
2024-03-25 17:10:45 +0100

    ah push it

--- src/gui/menu.cpp
+++ src/gui/menu.cpp
@@ -3,22 +3,25 @@
 #include "globals.h"
 
 void Menu::Render() {
-	ImGui::Begin("HellDivers2Menu", &ShowMenu, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar);
+	ImGui::Begin("Helldivers2Menu", &ShowMenu, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar);
 	ImGui::SetWindowSize(ImVec2(300, 300));
 
-	ImGui::Checkbox("Sample Amount", &fManipulateSamplePickup);
+	ImGui::Checkbox("Modify Sample Pickup Amount", &fManipulateSamplePickup);
 	ImGui::DragInt("##sampleamount", &SampleAmount, 1.0f, 1, 50);
 
-	ImGui::Spacing();
+	ImGui::Separator();
 
-	ImGui::Checkbox("Stratagem Unlock", &fStratagemUnlock);
-	ImGui::Checkbox("Armor Unlock", &fArmorUnlock);
-	ImGui::Checkbox("Equipment Unlock", &fEquipmentUnlock);
-	ImGui::Checkbox("Routine Deployment", &fRoutineDeployment);
-	ImGui::Checkbox("Weapon Ammo Reduction", &fWeaponAmmoReduction);
-	ImGui::Checkbox("Stratagem Reduction", &fStratagemReduction);
-	ImGui::Checkbox("Stamina Reduction", &fStaminaReduction);
-	ImGui::Checkbox("Enemy Basic Damage", &fEnemyBasicDamage);
+	ImGui::Checkbox("Unlock All Stratagems", &fStratagemUnlock);
+	ImGui::Checkbox("Unlock All Armor", &fArmorUnlock);
+	ImGui::Checkbox("Unlock All Equipments/Warbonds", &fEquipmentUnlock);
+
+	ImGui::Separator();
+
+	ImGui::Checkbox("No Stratagem/Shuttle Timer", &fRoutineDeployment);
+	ImGui::Checkbox("Unlimited Ammo", &fWeaponAmmoReduction);
+	ImGui::Checkbox("Unlimited Stratagems", &fStratagemReduction);
+	ImGui::Checkbox("Unlimited Stamina", &fStaminaReduction);
+	ImGui::Checkbox("GodMode", &fEnemyBasicDamage);
 
 	ImGui::End();
 }
